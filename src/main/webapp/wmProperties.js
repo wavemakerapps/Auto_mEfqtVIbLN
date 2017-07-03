50264281e5e5043f74a8302e0ac04c99f0ed7896
root <root@61949b3d2f95>
2017-07-03 05:40:41 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_mEfqtVIbLN",
+  "homePage" : "Main",
+  "name" : "Auto_mEfqtVIbLN",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
